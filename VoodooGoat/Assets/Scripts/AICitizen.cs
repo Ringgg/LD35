dc70b33cf8d71f00cd08ffd334bb9af7bf7a6365
195615 Kluchciński Bartosz <195615@edu.p.lodz.pl>
2016-04-16 14:24:17 +0200

    added Citizen Movement

--- VoodooGoat/Assets/Scripts/AICitizen.cs
+++ VoodooGoat/Assets/Scripts/AICitizen.cs
@@ -1,7 +1,33 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class AICitizen : MonoBehaviour
 {
-    Vector3 lastKnownLocation;
+    public Vector3 lastKnownLocation;
     float locationUpdateTme;
+    public bool isMoving = true;
+
+    AICharacterControl controller;
+
+    public Transform[] targetList;
+
+    void Start()
+    {
+        controller = GetComponent<AICharacterControl>();
+        lastKnownLocation = transform.position;
+        controller.target = targetList[Random.Range(0, targetList.Length - 1)];
+    }
+
+    public void SetNewTarget()
+    {
+        if (!isMoving)
+        {
+            do
+            {
+                controller.target = targetList[Random.Range(0, targetList.Length - 1)];
+            } while (controller.target.position == lastKnownLocation);
+
+            isMoving = true;
+        }
+    }
 }
---
+++ VoodooGoat/Assets/Scripts/CitizenTarget.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CitizenTarget : MonoBehaviour {
+
+    AICitizen citizen;
+    float waitTime =5;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        citizen = other.GetComponent<AICitizen>();
+        if(citizen != null)
+        {
+            StartCoroutine(Stay());
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        citizen = null;
+    }
+
+    IEnumerator Stay()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        citizen.lastKnownLocation = transform.position;
+        citizen.isMoving = false;
+        citizen.SetNewTarget();
+    }
+}
