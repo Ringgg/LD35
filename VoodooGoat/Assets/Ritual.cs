829004a4de1b3129b078883edbeafbbf1c9c973d
195615 Kluchciński Bartosz <195615@edu.p.lodz.pl>
2016-04-16 11:57:27 +0200

    rituals wip

---
+++ VoodooGoat/Assets/Ritual.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ritual : MonoBehaviour {
+
+    bool done;
+    float
+
+    void OnTriggerStay(Collider other)
+    {
+        if()
+        {
+            DoRitual();
+        }
+    }
+
+    IEnumerator DoRitual()
+    {
+        yield return new WaitForSeconds()
+    }
+}
