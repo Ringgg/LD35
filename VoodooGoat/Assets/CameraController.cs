7c451f2992563ebe5a9e95a55bbb8e20f79cb79f
195615 Kluchciński Bartosz <195615@edu.p.lodz.pl>
2016-04-16 11:55:58 +0200

    added test level

---
+++ VoodooGoat/Assets/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+        transform.position = target.position + new Vector3(0,5,-2);
+	}
+}
