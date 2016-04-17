0eb9a0ab8431a7b59169f2a3309ce320c8f7adb4
ringg <ringlan@gmail.com>
2016-04-17 17:37:46 +0200

    added npc meshes. added food. added main menu.

---
+++ VoodooGoat/Assets/Scripts/Food.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player" && Game.instance.player.state == Player.State.goat)
+        {
+            Game.instance.player.remainingShiftsBack += 1;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ VoodooGoat/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
